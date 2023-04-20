0c397a7210bc4c1e50ea2ae540b052bec0994080
donCelis <alexcelis019@gmail.com>
2023-04-19 22:29:47 -0500

    operadores en js

---
+++ javascript/4operadores.js
@@ -0,0 +1,12 @@
+/* Operadores matemáticos */
+// Suma -> (+)
+// Resta -> (-)
+// Multiplicación -> (*)
+// División (/)
+// Modulo (%) 10/3 -> 1
+// Exponencial (**)
+
+console.log(3 + 2);
+console.log(2 + "a");
+console.log(3 - 2);
+console.log(3 - "a"); // NaN -> Not a number
