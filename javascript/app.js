d3c7d396e7cf9b8558fb366a9290674f0c7a14b6
donCelis <alexcelis019@gmail.com>
2023-04-19 21:13:56 -0500

    carga diferida de archivos js

---
+++ javascript/app.js
@@ -0,0 +1,51 @@
+// comentario a una línea
+/* comentarios
+    a doble línea
+    */
+// los comentarios se utilizan para documentar el código
+/* Variables
+   Son elementos guardados en memoria ram, en donde puedo guardar cualquier cosa */
+
+/* conceptos en js para la creación de variables */
+/* declarar (let / const) -> crear una variable, solo se puede declarar una variable una vez */
+/* asignar (=) -> guardar un dato en un variable */
+/* llamar -> cuando uso la variable */
+/* reasignar (=) -> cambiar el valor de una variable ya declarada */
+
+let nombreUsuario = "Odilia Morales";
+console.log(nombreUsuario);
+
+let Nombre_Usuario = "Issa Garcia";
+// console.log(Nombre_Usuario);
+
+nombreUsuario = "Horacio Alvarez";
+console.log(nombreUsuario);
+
+let anioActual = 2023;
+
+const PI = 3.1416;
+
+// let nuevoPI = PI;
+// nuevoPI = 20;
+
+/* reglas para declarar nombres de variables */
+// let nombre usuario
+// let nombre-Usuario
+// let 1nombre_usuario
+// let ?nombreusuario
+// let añó
+
+/* Tipos de datos */
+/* 
+  Primitivos
+  string -> "hola mundo"
+  number -> 10.12
+  boolean -> true / false
+*/
+
+const mensaje = "Hola mundo";
+const dni = 129381723918237;
+const mayorEdad = true;
+
+/* debilmente tipado */
+/* estrictamene tipado */
